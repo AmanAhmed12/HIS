f5332ae6f6733cb858635e44aa10fcdf9e52aea3
AmanAhmed12 <amaanhilmy8@gmail.com>
2026-07-09 20:21:14 +0530

    Initial Angular project setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('HIS');
+  protected readonly title = signal('KIS');
+  count = signal(0);
+
+  increment() {
+    this.count.update(value => value + 1);
+  }
 }
