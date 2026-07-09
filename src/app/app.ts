fc8b60c8c5109c05e254b2945440d1baa55439d8
AmanAhmed12 <amaanhilmy8@gmail.com>
2026-07-09 22:16:16 +0530

    feat: implement landing page layout including navbar, footer, and features sections for about, gallery, faq, and contact.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './layouts/navbar/navbar';
+import { Home } from './features/landing/home/home';
+import { About } from './features/landing/about/about';
+import { Contact } from './features/landing/contact/contact';
+import { Faq } from './features/landing/faq/faq';
+import { Gallery } from './features/landing/gallery/gallery';
+import { Footer } from './layouts/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Home, About, Contact, Faq, Gallery, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
